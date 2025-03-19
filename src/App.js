c1bf0363bce277ad78f33acc88be9ba71572df8c
Amanda Berwig <amandaberwig@gmail.com>
2025-03-18 21:59:34 -0300

    Add Time component to display team names and integrate into App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import { useState } from "react";
 import Banner from "./components/Banner/Banner";
 import Formulario from "./components/Formulario/Formulario";
+import Time from "./components/Time/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "var(--destaq-prog)",
+      corSecundaria: "var(--fundo-prog)",
+    },
+    {
+      nome: "Front-End",
+      corPrimaria: "var(--destaq-front)",
+      corSecundaria: "var(--fundo-front)",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "var(--destaq-data)",
+      corSecundaria: "var(--fundo-data)",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "var(--destaq-devops)",
+      corSecundaria: "var(--fundo-devops)",
+    },
+    {
+      nome: "UX e Design ",
+      corPrimaria: "var(--destaq-ux)",
+      corSecundaria: "var(--fundo-ux)",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "var(--destaq-mobile)",
+      corSecundaria: "var(--fundo-mobile)",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "var(--destaq-inov)",
+      corSecundaria: "var(--fundo-inov)",
+    },
+  ];
   const [colaboradores, setColaboradores] = useState([]);
 
   return (
@@ -14,6 +52,9 @@ function App() {
           setColaboradores([...colaboradores, colaborador]);
         }}
       />
+      {times.map((time) => (
+        <Time nome={time.nome} />
+      ))}
     </div>
   );
 }
